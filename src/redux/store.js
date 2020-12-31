40d2204213827dcdff6afbdd9e94915c2603d543
Yash Sanghavi <38398261+yash001dev@users.noreply.github.com>
2020-12-31 12:05:20 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,3 +6,6 @@ const middleware = [logger];
 const store = createStore(rootReducer, applyMiddleware(...middleware))
 
 export default store;
+
+//In this Project why you Define Redux....
+//If you fetching Data from Frontend React Component than doesn't need of REDUX.
